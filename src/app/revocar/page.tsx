366a4df3c3c91b28b2cdb4e6950734378ea77a45
milogodoy <milogodoy@hotmail.com>
2025-03-26 10:26:20 -0400

    agregando revocar

---
+++ src/app/revocar/page.tsx
@@ -0,0 +1,15 @@
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import React from 'react'
+
+const RevocarPage = () => {
+  return (
+    <div>
+      <Header />
+      <h1 className='font-bold text-center'>Sitio en Construcción</h1>
+      <Footer />
+    </div>
+  )
+}
+
+export default RevocarPage
